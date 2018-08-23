a0e94e2e72a4149cf9d35190b0b98b9f5517ad37
Pratik Kshirsagar <pratikkshirsagar@Pratiks-MBP.hsd1.il.comcast.net>
2018-08-22 20:47:43 -0500

    add first ajax call

--- Path_to_React/Node_API/public/app.js
+++ Path_to_React/Node_API/public/app.js
@@ -4,15 +4,39 @@ $(document).ready(function() {
     .catch(function() {
         console.log('error occurred.');
     });
+
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13) {
+            //create todo
+            createTodo();
+        }
+    });
 });
 
 function addTodos(todos) {
     todos.forEach(function (todo){
-        console.log(todo.name);
+        addTodo(todo);
+    });
+} 
+
+function addTodo(todo) {
+    console.log(todo.name);
         var newTodo = $('<li class="task">' + todo.name + '</li>');
         if(todo.completed) {
             newTodo.addClass('done');
         }
         $('.list').append(newTodo);
+}
+
+function createTodo() {
+    //send request to create new todo
+    var userInput =  $('#todoInput').val();
+    $.post('/api/todos', {name: userInput})
+    .then(function(newTodo) {
+        $('#todoInput').val('');        
+        addTodo(newTodo);
+    })
+    .catch(function(err) {
+        console.log(err);
     });
-} 
+}
